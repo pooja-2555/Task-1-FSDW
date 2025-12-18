6e0a07df364695f7d90d5ee205817370f990e153
pooja-2555 <indirapooja2005@gmail.com>
2025-12-18 14:45:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Dashboard from "./Fullstack/Sample/Dashboard";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+export default App;
