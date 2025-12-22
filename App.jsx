f80968595ee8dc75d7ca9e26a73a00c67943df22
pooja-2555 <indirapooja2005@gmail.com>
2025-12-22 17:03:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OnlineCab from "./Fullstack/Sample/Online cab";
+
+function App() {
+  return (
+    <div>
+      <OnlineCab />
+    </div>
+  );
+}
+
+export default App;
