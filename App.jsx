1b957b076b40fdb5af15f862dc42dcafb259c79c
pooja-2555 <indirapooja2005@gmail.com>
2025-12-31 16:23:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RegistrationForm from "./Fullstack/Sample/Registrationform";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <RegistrationForm />
+      </div>
+    );
+  }
+}
+
+export default App;
